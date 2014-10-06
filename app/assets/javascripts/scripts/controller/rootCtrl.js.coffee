a1de22c323e6c6682b91da06b326338bceb7fd98
owen8505 <luis.sanchez.franco@gmail.com>
2014-10-06 12:34:25 -0500

    Merge

--- app/assets/javascripts/scripts/controller/rootCtrl.js.coffee
+++ app/assets/javascripts/scripts/controller/rootCtrl.js.coffee
@@ -1,11 +1,3 @@
 @Geek.controller 'RootController', ($scope, $rootScope, $cookieStore, CountyService, DEFAULT_VALUES)->
-
-  $rootScope.laguage = $cookieStore.get('language')
-
-  if $rootScope.language
-    console.log $rootScope.language
-  else
-    $rootScope.language = DEFAULT_VALUES.LANGUAGE
-
-  $scope.counties = CountyService.getAllItems()
+  console.log 'ENTRE'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,9 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
-  config.assets.precompile += %w( zjquery-ui.js )
-  config.assets.precompile += %w( autocomplete.css )
-  config.assets.precompile += %w( autocomplete.js )
-  config.assets.precompile += %w( scripts/controller/rootCtrl.js )
+  config.assets.precompile += %w( scripts/controller/pruebaCtrl.js )
+
 
 end
