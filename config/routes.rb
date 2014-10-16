820a40c5fa3c68facf950d46e04b4fc8ca44c154
tonklis <benjamin.hernandez@in2teck.com>
2014-10-16 16:06:12 -0500

    Faltaba quitar HEAD del merge.

--- config/routes.rb
+++ config/routes.rb
@@ -85,6 +85,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'student_display#landing'
+  root 'student_display#landing'
 
 end
