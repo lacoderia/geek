7692346b8c40d1f5a0fb48fe3916e181a074faa5
tonklis <benjamin.hernandez@in2teck.com>
2014-10-30 16:58:48 -0600

    Version nueva de assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.serve_static_assets = true 
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
   # config.assets.js_compressor = Uglifier.new(mangle: false)
 
