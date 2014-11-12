830edf8dc7419a5865474341f71ce4db7972e9b7
tonklis <benjamin.hernandez@in2teck.com>
2014-11-12 17:54:00 -0600

    Agregado default #19812712196134 de costo

--- db/migrate/20141112225607_add_id_and_cost_to_categories_tutors.rb
+++ db/migrate/20141112225607_add_id_and_cost_to_categories_tutors.rb
@@ -1,6 +1,6 @@
 class AddIdAndCostToCategoriesTutors < ActiveRecord::Migration
   def change
     add_column :categories_tutors, :id, :primary_key
-    add_column :categories_tutors, :cost, :float
+    add_column :categories_tutors, :cost, :float, :default => 0
   end
 end
