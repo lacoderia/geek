b54fa6b5577a159cb1965f7c796bf7719fd9148a
schultzRr <rosas_schultz@hotmail.com>
2014-11-12 13:18:59 -0600

    Se corrigió el método que cierra las alertas en la forma del perfil del tutor

--- app/assets/javascripts/lib/angular/angular-form-alert-directive.js
+++ app/assets/javascripts/lib/angular/angular-form-alert-directive.js
@@ -6,11 +6,15 @@ Geek.directive('ngAlert', ["$rootScope", function($rootScope){
         replace: false,
         transclude: false,
         template:   '<div class="alert" ng-class="ALERT_VALUES[alertParams.type].alertClass" role="alert" ng-show="alertParams">' +
-                        '<span ng-show="alertParams.icon" class="alert-icon" ng-class="ALERT_VALUES[alertParams.type].alertIcon"></span> {{ alertParams.message }} <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                        '<span ng-show="alertParams.icon" class="alert-icon" ng-class="ALERT_VALUES[alertParams.type].alertIcon"></span> {{ alertParams.message }} <button type="button" class="close" ng-click="closeAlert()"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
                     '</div>',
         link: function(scope, element, attrs){
 
             scope.ALERT_VALUES = {'warning': { alertClass: 'alert-warning', alertIcon: 'icon-alert'}, 'info' : { alertClass: 'alert-info', alertIcon: ''}, 'success': { alertClass: 'alert-success', alertIcon: 'icon-accept'}, 'danger': { alertClass: 'alert-danger', alertIcon: 'icon-alert'}};
+
+            scope.closeAlert = function() {
+                scope.alertParams = null;
+            }
 
         },
         scope: {
