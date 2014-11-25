020a2ff27feff99fcd728d6e09e2b09138097d6f
owen8505 <luis.sanchez.franco@gmail.com>
2014-11-25 12:18:32 -0600

    Se agregó google places para las búsquedas de las zonas

--- app/assets/javascripts/student/services/tutorService.js
+++ app/assets/javascripts/student/services/tutorService.js
@@ -30,16 +30,14 @@ Geek.factory('TutorService', ["$http", "$q", "DEFAULT_VALUES", function($http, $
         return promise
     };
 
-    var getTutorByQueryParamsForGoogle = function(queryParams){
+    var getTutorByQueryParamsForGoogle = function(queryParams, categoryId){
         var deferred = $q.defer();
         var promise = deferred.promise;
 
-        console.log(queryParams)
-
         $http.post(DEFAULT_VALUES.URL_SERVICES.TUTOR_BY_GOOGLE_SERVICE_URL,{
             category_str: null,
-            category_id: null,
-            zone_obj: queryParams,
+            category_id: categoryId,
+            zone_obj: queryParams
         }).
             success(function(data){
                 deferred.resolve(data);
