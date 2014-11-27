e63b2a561a4fccf82583c6eec09772a9bfc91e58
tonklis <benjamin.hernandez@in2teck.com>
2014-11-27 14:27:57 -0600

    Servicios para reportar anomalías #21221372315367

--- app/controllers/registered_anomalies_controller.rb
+++ app/controllers/registered_anomalies_controller.rb
@@ -1,2 +1,23 @@
-class RegisteredAnomaliesController < InheritedResources::Base
+class RegisteredAnomaliesController < ApplicationController
+
+  # Recibe:
+  # appointment_id = id de la cita
+  # anomaly_code = código de la anomalía (0 late show, 1 no show, 2 cancelada, 3 otro)
+  # description = descripción de la anomalía
+  # Regresa:
+  # El objeto de registered anomaly en caso que se cree correctamente, nil de lo contrario
+  def from_student
+    @registered_anomaly = RegisteredAnomaly.from_student(params[:appointment_id], params[:anomaly_code], params[:description])
+  end
+
+  # Recibe:
+  # appointment_id = id de la cita
+  # anomaly_code = código de la anomalía (0 late show, 1 no show, 2 cancelada, 3 otro)
+  # description = descripción de la anomalía
+  # Regresa:
+  # El objeto de registered anomaly en caso que se cree correctamente, nil de lo contrario
+  def from_tutor
+    @registered_anomaly = RegisteredAnomaly.from_tutor(params[:appointment_id], params[:anomaly_code], params[:description])
+  end
+
 end
---
+++ app/views/registered_anomalies/from_student.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @registered_anomaly, :id, :description, :user_id, :source_id, :appointment_id, :registered_anomaly_status_id
