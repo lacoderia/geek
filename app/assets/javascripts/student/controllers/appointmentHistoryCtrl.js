d87c0d94d0e284eaad12b925fc3319314a9ff8bc
schultzRr <rosas_schultz@hotmail.com>
2014-12-16 18:55:33 -0600

    Se agregaron traducciones a los templates del estudiante

--- app/assets/javascripts/student/controllers/appointmentHistoryCtrl.js
+++ app/assets/javascripts/student/controllers/appointmentHistoryCtrl.js
@@ -47,10 +47,7 @@ Geek.controller('AppointmentHistoryController',['$filter', '$scope', '$rootScope
                         var groupDateArray = groupDateKey.split('-');
                         var groupDate = new Date(parseInt(groupDateArray[0]), parseInt(groupDateArray[1])-1, parseInt(groupDateArray[2]));
                         var appointmentGroup = {
-                            numberDay: groupDate.getDate(),
-                            day: groupDate.getDay(),
-                            month: groupDate.getMonth(),
-                            year: groupDate.getYear(),
+                            date: groupDate.toString(),
                             appointments: $scope.getAppointments(data[groupIndex][groupDateKey])
                         };
                         $scope.appointmentsGroups.push(appointmentGroup);
