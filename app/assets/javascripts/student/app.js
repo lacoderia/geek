a2844dbc04b9f43ffc963b0fcd4c373a8f798014
schultzRr <rosas_schultz@hotmail.com>
2014-12-17 18:45:19 -0600

    Se agregó el servicio que obtiene los tutores a los que debe calificar el estudiante

--- app/assets/javascripts/student/app.js
+++ app/assets/javascripts/student/app.js
@@ -16,6 +16,7 @@ var Geek = angular.module('Geek', ['ngResource', 'ngRoute', 'angucomplete-alt-ge
             'MESSAGE_SAVE_SERVICE_URL': 'messages.json',
             'MESSAGES_BY_CONVERSATION_SERVICE_URL': 'messages/by_conversation.json',
             'MARK_AS_SERVICE_URL': 'messages/mark_read.json',
+            'MY_TUTORS_SERVICE_URL': '/tutors/by_student.json'
         },
         'HOURS': [  '00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30',
             '04:00', '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30',
