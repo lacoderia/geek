04cafb434896209b3b9174a6cf8255566dd5f7f5
tonklis <benjamin.hernandez@in2teck.com>
2015-01-02 12:30:59 -0600

    Cerrada funcionalidad de pago manual #23119959184419

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
 
-	def self.register_card user, token
+  def self.register_card user, token
     user_openpay_id = user.get_openpay_id
     result = Payment.add_card(user_openpay_id, token)
     card = nil
