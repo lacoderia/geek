fcecb2efe0fc927d38b3a8159b158f36909330f7
dmiramon <dmiramon@gmail.com>
2015-01-19 18:21:23 -0600

    B2-Bloqueo de usuarios: Revisar estado en login  #23641302358986
    Diego

--- app/controllers/student_display_controller.rb
+++ app/controllers/student_display_controller.rb
@@ -1,17 +1,12 @@
 class StudentDisplayController < ApplicationController
 
   def landing
-    if session["errors.inactive"] 
-      session["errors.inactive"] = nil 
-      @error = true
-    else
-      if session["devise.facebook_data"]
-        auth_hash = session["devise.facebook_data"]
-        role = Role.find_by_name("student")
-        @user = User.new(:first_name => auth_hash["info"]["first_name"], :last_name => auth_hash["info"]["last_name"], :uid => auth_hash["uid"], :email => auth_hash["info"]["email"], :token => auth_hash["credentials"]["token"], :refresh_token => auth_hash["credentials"]["refresh_token"], :roles => [role])
-      elsif session["student.login"]
-        @login = true
-      end
+    if session["devise.facebook_data"]
+      auth_hash = session["devise.facebook_data"]
+      role = Role.find_by_name("student")
+      @user = User.new(:first_name => auth_hash["info"]["first_name"], :last_name => auth_hash["info"]["last_name"], :uid => auth_hash["uid"], :email => auth_hash["info"]["email"], :token => auth_hash["credentials"]["token"], :refresh_token => auth_hash["credentials"]["refresh_token"], :roles => [role])
+    elsif session["student.login"]
+      @login = true
     end
   end
 
