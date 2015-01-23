568fa29e2df486dde6f48a703446cac84b75b40b
tonklis <benjamin.hernandez@in2teck.com>
2015-01-23 13:35:49 -0600

    Faltaba incluir municipios en la BD #24983978629589

---
+++ db/migrate/20150123191445_add_municipalities_if_empty.rb
@@ -0,0 +1,15 @@
+class AddMunicipalitiesIfEmpty < ActiveRecord::Migration
+  def change
+    file_df = File.open('db/sepomex/zonas_depuradas.txt', 'r:UTF-8')
+    while (line = file_df.gets)
+      arr = line.encode!('UTF-8', 'UTF-8', :invalid => :replace).split("|")
+      municipio = Municipality.find_or_create_by(name: arr[3])
+      if not cp = PostalCode.find_by_code(arr[0])
+        cp = PostalCode.create(code: arr[0], state_id: estado.id, city_id: ciudad.id, municipality_id: municipio.id)
+      elsif not cp.municipality_id
+        cp.update_attribute(:municipality_id, municipio.id)
+      end
+    end
+    file_df.close
+  end
+end
