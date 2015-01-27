5659c89f68d322b7bd2a8d33916b3ae150ff9201
tonklis <benjamin.hernandez@in2teck.com>
2015-01-26 19:06:40 -0600

    Agregadas migraciones para conservar los precios #25202288983905

--- db/migrate/20150115190853_fix_categories.rb
+++ db/migrate/20150115190853_fix_categories.rb
@@ -4,6 +4,10 @@ class FixCategories < ActiveRecord::Migration
       category_to_keep = Category.where("name = ? AND id <> ?", I18n.transliterate(category.name).upcase, category.id).first
       if category_to_keep
         category_to_keep.tutors << category.tutors
+        category.categories_tutors.each do |ct|
+          CategoriesTutor.create(category_id: ct.category_id, tutor_id: ct.tutor, cost: ct.cost)
+        end
+        category.categories_tutors.destroy_all
         category.destroy
       else
         category.update_attribute(:name, I18n.transliterate(category.name).upcase)
