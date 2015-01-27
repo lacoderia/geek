cf1b037f6060bbdd386eb69f7035ba119b783205
tonklis <benjamin.hernandez@in2teck.com>
2015-01-27 14:05:39 -0600

    Arreglado bug de migración

--- db/migrate/20150123191445_add_municipalities_if_empty.rb
+++ db/migrate/20150123191445_add_municipalities_if_empty.rb
@@ -3,6 +3,8 @@ class AddMunicipalitiesIfEmpty < ActiveRecord::Migration
     file_df = File.open('db/sepomex/zonas_depuradas.txt', 'r:UTF-8')
     while (line = file_df.gets)
       arr = line.encode!('UTF-8', 'UTF-8', :invalid => :replace).split("|")
+      estado = State.find_or_create_by(name: arr[4])
+      ciudad = City.find_or_create_by(name: arr[5])
       municipio = Municipality.find_or_create_by(name: arr[3])
       if not cp = PostalCode.find_by_code(arr[0])
         cp = PostalCode.create(code: arr[0], state_id: estado.id, city_id: ciudad.id, municipality_id: municipio.id)
