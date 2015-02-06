87646362cb02178d5b126265da77ee0aeadb1533
dmiramon <dmiramon@gmail.com>
2015-02-05 20:32:34 -0600

    Cambio para tener producción de Openpay como variable de ambiente
    Diego

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -266,7 +266,7 @@ class Payment
   end
 
   def self.set_openpay
-    openpay = OpenpayApi.new(ENV['OPENPAY_CLIENT_ID'], ENV['OPENPAY_CLIENT_SECRET'], true)
+    openpay = OpenpayApi.new(ENV['OPENPAY_CLIENT_ID'], ENV['OPENPAY_CLIENT_SECRET'], ENV['OPENPAY_IS_PRODUCTION'] == '1')
   end
   
   private_class_method :set_openpay
