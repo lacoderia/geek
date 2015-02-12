40ff64846e5883e6b895412b41540bfafaddb9d3
dmiramon <dmiramon@gmail.com>
2015-02-11 19:09:56 -0600

    Cambios a devise para manejar el cambio de password Diego

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,7 @@
 class Users::PasswordsController < Devise::PasswordsController
-	
+  
+  skip_before_filter :verify_authenticity_token, :only => [:create, :update]
+
 	def create
 		self.resource = resource_class.send_reset_password_instructions(resource_params)
     yield resource if block_given?
