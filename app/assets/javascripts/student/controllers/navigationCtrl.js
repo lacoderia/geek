a291511b7c68355dd3e1ef5a3741312f923a5c70
schultzRr <rosas_schultz@hotmail.com>
2015-02-12 14:45:50 -0600

    GT-06: Recuperar contraseña de usuarios
    Se agregó la funcionalidad completa
    
    #26201134737520

--- app/assets/javascripts/student/controllers/navigationCtrl.js
+++ app/assets/javascripts/student/controllers/navigationCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES", function($scope, $rootScope, DEFAULT_VALUES){
+Geek.controller('NavigationController', ["$filter", "$scope", "$rootScope", "$timeout", "ForgotService", "DEFAULT_VALUES", "usSpinnerService", function($filter, $scope, $rootScope, $timeout, ForgotService, DEFAULT_VALUES, usSpinnerService){
 
     $scope.selectedTab = 'signUp';
 
@@ -12,12 +12,12 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
     $scope.showSignInModal = function(selectedTab) {
         $scope.selectTab(selectedTab);
         $('#sign-in-modal').modal('show');
-    }
+    };
 
     //Function that selects the sign in modal tab
     $scope.selectTab = function(selectedTab) {
         $scope.selectedTab = selectedTab;
-    }
+    };
 
     // Función que envía la forma de registro del estudiante después de validarla
     $scope.submitStudentForm = function(){
@@ -26,7 +26,7 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
         if ($scope.signUpForm.$valid) {
             $("#student-form").submit();
         }
-    }
+    };
 
     // Función que envía la forma de registro del estudiante después de validarla
     $scope.loginStudent = function(){
@@ -35,10 +35,89 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
         if ($scope.studentLoginForm.$valid) {
             $("#student-login-form").submit();
         }
-    }
+    };
 
     $scope.registerFacebook = function(){
         window.location = "users/auth/facebook";
+    };
+
+    $scope.showForgotPassword = function(){
+        $scope.forgotPasswordMessage = '';
+        $scope.forgotPasswordError = '';
+        $scope.selectTab('forgotPassword');
+    };
+
+    $scope.sendForgotPasswordEmail = function(){
+        $scope.forgotPasswordMessage = '';
+        $scope.forgotPasswordError = '';
+
+        $timeout(function(){
+            usSpinnerService.spin('forgot-password-spinner');
+        }, 0);
+
+        if($scope.forgotPasswordEmail){
+            ForgotService.sendForgotPassword($scope.forgotPasswordEmail).then(
+                function(data){
+                    if (data.success){
+                        $scope.forgotPasswordMessage = $filter('translate')('FORGOT_PASSWORD_SENT');
+                    } else {
+                        $scope.forgotPasswordError = $filter('translate')('ERROR_FORGOT_PASSWORD');
+                    }
+                },
+                function (response){
+                    $scope.forgotPasswordError = $filter('translate')('ERROR_FORGOT_PASSWORD');
+                    console.log('Error sending email: ' + response);
+                }
+            ).finally(function(){
+                    $timeout(function(){
+                        usSpinnerService.stop('forgot-password-spinner');
+                    }, 0);
+                });
+        }
+
+    };
+
+    $scope.showResetPassword = function(){
+        $scope.resetPasswordMessage = '';
+        $scope.resetPasswordError = '';
+        $scope.selectTab('resetPassword');
+    };
+
+    $scope.sendResetPassword = function(){
+        $scope.resetPasswordMessage = '';
+        $scope.resetPasswordError = '';
+
+        $timeout(function(){
+            usSpinnerService.spin('reset-password-spinner');
+        }, 0);
+
+        var user = {
+            "reset_password_token": $rootScope.resetToken,
+            "password": $scope.resetPasswordPassword,
+            "password_confirmation": $scope.resetPasswordConfirmation
+        }
+
+        $scope.$broadcast('show-errors-check-validity', $scope.resetPasswordForm);
+
+        if ($scope.resetPasswordForm.$valid) {
+            ForgotService.sendResetPassword(user).then(
+                function(data){
+                    if (data.success){
+                        $scope.resetPasswordMessage = $filter('translate')('RESET_PASSWORD_SENT');
+                    } else {
+                        $scope.resetPasswordError = $filter('translate')('ERROR_RESET_PASSWORD');
+                    }
+                    usSpinnerService.stop('reset-password-spinner');
+                },
+                function (response){
+                    $scope.resetPasswordError = $filter('translate')('ERROR_RESET_PASSWORD');
+                    console.log('Error resetting password: ' + response);
+                }
+            ).finally(function(){
+                    usSpinnerService.stop('reset-password-spinner');
+                });
+        }
+
     };
 
     $scope.$on("rootControllerReady", function() {
@@ -59,6 +138,11 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
         if ($("#login-data").data()) {
             $scope.showSignInModal('signIn'); 
             $scope.loginError = $("#login-data").data('error');
+        }
+
+        if($rootScope.resetToken){
+            $scope.selectTab('resetPassword');
+            $('#sign-in-modal').modal('show');
         }
     });
 }]);
--- app/assets/javascripts/tutor/controllers/navigationCtrl.js
+++ app/assets/javascripts/tutor/controllers/navigationCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES", function($scope, $rootScope, DEFAULT_VALUES){
+Geek.controller('NavigationController', ["$filter", "$scope", "$rootScope", "$timeout", "ForgotService", "DEFAULT_VALUES", "usSpinnerService", function($filter, $scope, $rootScope, $timeout, ForgotService, DEFAULT_VALUES, usSpinnerService){
 
     $scope.selectedTab = 'signUp';
  
@@ -30,12 +30,12 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
     $scope.showSignInModal = function(selectedTab) {
         $scope.selectTab(selectedTab);
         $('#sign-in-modal').modal('show');
-    }
+    };
 
     // Function that selects the sign in modal tab
     $scope.selectTab = function(selectedTab) {
         $scope.selectedTab = selectedTab;
-    }
+    };
 
     // Función que envía la forma de registro del tutor después de validarla
     $scope.submitTutorForm = function(){
@@ -44,7 +44,7 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
         if ($scope.signUpForm.$valid) {
             $("#tutor-form").submit();
         }
-    }
+    };
 
     // Función que envía la forma de registro del tutor después de validarla
     $scope.loginTutor = function(){
@@ -53,10 +53,93 @@ Geek.controller('NavigationController', ["$scope", "$rootScope", "DEFAULT_VALUES
         if ($scope.tutorLoginForm.$valid) {
             $("#tutor-login-form").submit();
         }
-    }
+    };
 
     $scope.registerGooglePlus = function(){
         window.location = "users/auth/google_oauth2";
     };
+
+    $scope.showForgotPassword = function(){
+        $scope.forgotPasswordMessage = '';
+        $scope.forgotPasswordError = '';
+        $scope.selectTab('forgotPassword');
+    };
+
+    $scope.sendForgotPasswordEmail = function(){
+        $scope.forgotPasswordMessage = '';
+        $scope.forgotPasswordError = '';
+
+        $timeout(function(){
+            usSpinnerService.spin('forgot-password-spinner');
+        }, 0);
+
+        if($scope.forgotPasswordEmail){
+            ForgotService.sendForgotPassword($scope.forgotPasswordEmail).then(
+                function(data){
+                    if (data.success){
+                        $scope.forgotPasswordMessage = $filter('translate')('FORGOT_PASSWORD_SENT');
+                    } else {
+                        $scope.forgotPasswordError = $filter('translate')('ERROR_FORGOT_PASSWORD');
+                    }
+                    usSpinnerService.stop('forgot-password-spinner');
+                },
+                function (response){
+                    $scope.forgotPasswordError = $filter('translate')('ERROR_FORGOT_PASSWORD');
+                    console.log('Error sending email: ' + response);
+                }
+            ).finally(function(){
+                    usSpinnerService.stop('forgot-password-spinner');
+                });
+        }
+
+    };
+
+    $scope.showResetPassword = function(){
+        $scope.resetPasswordMessage = '';
+        $scope.resetPasswordError = '';
+        $scope.selectTab('resetPassword');
+    };
+
+    $scope.sendResetPassword = function(){
+        $scope.resetPasswordMessage = '';
+        $scope.resetPasswordError = '';
+
+        $timeout(function(){
+            usSpinnerService.spin('reset-password-spinner');
+        }, 0);
+
+        var user = {
+            "reset_password_token": $rootScope.resetToken,
+            "password": $scope.resetPasswordPassword,
+            "password_confirmation": $scope.resetPasswordConfirmation
+        }
+
+        $scope.$broadcast('show-errors-check-validity', $scope.resetPasswordForm);
+
+        if ($scope.resetPasswordForm.$valid) {
+            ForgotService.sendResetPassword(user).then(
+                function(data){
+                    if (data.success){
+                        $scope.resetPasswordMessage = $filter('translate')('RESET_PASSWORD_SENT');
+                    } else {
+                        $scope.resetPasswordError = $filter('translate')('ERROR_RESET_PASSWORD');
+                    }
+                    usSpinnerService.stop('reset-password-spinner');
+                },
+                function (response){
+                    $scope.resetPasswordError = $filter('translate')('ERROR_RESET_PASSWORD');
+                    console.log('Error resetting password: ' + response);
+                }
+            ).finally(function(){
+                    usSpinnerService.stop('reset-password-spinner');
+                });
+        }
+
+    };
+
+    if($rootScope.resetToken){
+        $scope.selectTab('resetPassword');
+        $('#sign-in-modal').modal('show');
+    }
 
 }]);
